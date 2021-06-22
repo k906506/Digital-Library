be9775a4e40f519ff37b539d47bbe10fef14a694
k906506 <k906506@naver.com>
2021-06-22 17:08:31 +0900

    회원가입 절차 구현 완료

--- processOfSignUp.php
+++ processOfSignUp.php
@@ -1,23 +1,17 @@
 <?php
-$conn = mysqli_connect(주소, 아이디, "비밀번호");
+$conn = oci_connect('c##kodo', 1234); // USER_ACCOUNT_INFO에 email과 password를 등록한다,
 $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-echo $hashedPassword;
-$sql = "
-    INSERT INTO user
-    (email, password, created)
-    VALUES('{$_POST['email']}', '{$hashedPassword}', NOW()
-    )";
-echo $sql;
-$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO USER_ACCOUNT_INFO (email, password) VALUES('{$_POST['email']}', '{$hashedPassword}')";
+$result = oci_parse($conn, $sql);
 
 if ($result === false) {
-    echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
-    echo mysqli_error($conn);
+    echo "이미 존재하는 계정이거나 형식이 잘못되었습니다. 다시 시도해주세요.";
+    echo oci_connect($conn);
 } else {
     ?>
     <script>
         alert("회원가입이 완료되었습니다");
-        location.href = "index.php";
+        location.href = "main.php";
     </script>
     <?php
 }
