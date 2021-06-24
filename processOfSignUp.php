01470cbaa14170ff42869d0b923ce1aaced84163
k906506 <k906506@naver.com>
2021-06-24 16:39:25 +0900

    입력 값 수정으로 인한 쿼리문 변경

--- processOfSignUp.php
+++ processOfSignUp.php
@@ -15,7 +15,7 @@ $conn = oci_connect('d201701971', "rhehgus1019", $dsn); // DB와 연동
 // $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT); -> 암호화를 위한 과정이지만 암호화를 진행할 경우 자리수가 늘어나서 DB에 INSERT가 되지 않음
 
 // 1. EMAIL이 이미 사용 중인 경우 가입 불가
-$sqlFindPk = "SELECT * FROM USER_ACCOUNT_INFO WHERE EMAIL = '{$_POST['email']}'"; // 입력한 EMAIL이 이미 사용 중인지 검색
+$sqlFindPk = "SELECT * FROM CUSTOMER WHERE CNO = '{$_POST['cno']}'"; // 입력한 CNO가 이미 사용 중인지 검색
 $resultFindPk = oci_parse($conn, $sqlFindPk);
 oci_execute($resultFindPk);
 $storedPassword = oci_fetch_array($resultFindPk)[0]; // 이미 사용 중인 경우 storedPassWord에 값이 존재
@@ -30,7 +30,7 @@ if ($storedPassword) { // 이미 있는 경우
 }
 
 // 2. EMAIL이 사용 중이 아닌 경우 가입 가능
-$sql = "INSERT INTO USER_ACCOUNT_INFO (CNAME, PHONENUMBER, EMAIL, PASSWORD) VALUES ('{$_POST['name']}', '{$_POST['phoneNumber']}', '{$_POST['email']}', '{$_POST['password']}')";
+$sql = "INSERT INTO CUSTOMER (CNO, NAME, PASSWD, EMAIL) VALUES ('{$_POST['cno']}', '{$_POST['name']}', '{$_POST['password']}', '{$_POST['email']}')";
 $result = oci_parse($conn, $sql);
 oci_execute($result);
 oci_free_statement($result); // 메모리 반환
