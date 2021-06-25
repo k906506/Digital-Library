3f40fe8601f4913eacba104747b19297ab6878d7
k906506 <k906506@naver.com>
2021-06-25 13:56:58 +0900

    대여 기능 구현 완료

--- processOfBorrowBook.php
+++ processOfBorrowBook.php
@@ -22,16 +22,37 @@ if (oci_fetch_row($sql_info)) { // 대여 불가
     ?>
     <script>
         alert("이미 대여된 도서입니다. 예약을 진행해주세요.");
-        location.href = "secondMain.php";
+        location.href = history.back();
     </script>
     <?php
 } else { // 대여 가능
-    $sql = "INSERT INTO PREVIOUSRENTAL (ISBN, DATERENTED, DATERETURNED, CNO) VALUES ('{$_GET['isbn']}', SYSDATE, SYSDATE + 10,'{$_SESSION['userCno']}')";
-    ?>
-    <script>
-        alert("대여가 완료되었습니다.");
-        location.href = "secondMain.php";
-    </script>
-    <?php
+    $sql = "SELECT * FROM PREVIOUSRENTAL WHERE CNO = '{$_SESSION['userCno']}'";
+    $sql_info = oci_parse($conn, $sql);
+    oci_execute($sql_info);
+
+    $data = array();
+    while ($row = oci_fetch_row($sql_info)) {
+        $data[] = [$row[0], $row[1], $row[2], $row[3]];
+    }
+    $countReservation = sizeof($data); // 쿼리문 실행 결과의 크기
+
+    if ($countReservation > 2) {
+        ?>
+        <script>
+            alert("대여는 최대 3권까지 가능합니다.");
+            location.href = history.back();
+        </script>
+        <?php
+    } else {
+        $sql = "INSERT INTO PREVIOUSRENTAL (ISBN, DATERENTED, DATERETURNED, CNO) VALUES ('{$_GET['isbn']}', SYSDATE, SYSDATE + 10,'{$_SESSION['userCno']}')";
+        $sql_info = oci_parse($conn, $sql);
+        oci_execute($sql_info);
+        ?>
+        <script>
+            alert("대여가 완료되었습니다.");
+            location.href = "secondMain.php";
+        </script>
+        <?php
+    }
 }
 ?>
