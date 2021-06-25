5d7ac695ffcdf6395860685dc71270bcc49f03d2
k906506 <k906506@naver.com>
2021-06-25 13:56:12 +0900

    반납 기능 구현 완료

--- processOfReturnBookAfterClick.php
+++ processOfReturnBookAfterClick.php
@@ -1,4 +1,25 @@
 <?php
+// 인코딩
+putenv("NLS_LANG=KOREAN_KOREA.UTF8");
+
+// HOST 값은 C:\Windows\System32\drivers\etc 에서 확인 가능
+$dsn = "
+(DESCRIPTION=
+(ADDRESS_LIST= (ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))) 
+(CONNECT_DATA= (SERVICE_NAME=XE))
+)
+";
 session_start();
-echo $_SESSION['isbn'];
+
+// 실행
+$conn = oci_connect('d201701971', "rhehgus1019", $dsn); // DB와 연동
+
+// 이미 예약된 경우
+$sql = "DELETE FROM PREVIOUSRENTAL WHERE ISBN = '{$_GET['isbn']}'";
+$sql_info = oci_parse($conn, $sql);
+oci_execute($sql_info);
 ?>
+<script>
+    alert("반납되었습니다.");
+    location.href = history.back();
+</script>
